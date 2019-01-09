36b42e359ad4aca6a1f5f1cccf8a55cf35201859
MCPolichette <mark@polichette.com>
2019-01-09 08:44:21 -0700

    deleting defaults and preparing layout

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="container">
+          <div className="row" id="dealer-row">
+            <div className="col-lg-12">
+              <h1>Dealer's Cards and Display</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-6">
+              <h2>Player's cards</h2
+              ></div>
+          </div>
+        </div>
+
+
+
       </div>
     );
   }
